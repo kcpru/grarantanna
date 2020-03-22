215d2e8e85f8b60f1effd89cceffaa6103e175f4
JustABanana <JustABanana@users.noreply.github.com>
2020-03-22 17:38:07 +0100

    Add health and basic enemy

---
+++ Assets/Scripts/Enemies/BasicEnemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+///<summary>
+///A basic enemy that deals damage to the player on contact. Can be used for spikes or as a base class for another enemy.
+///</summary>
+public class BasicEnemy : MonoBehaviour
+{
+    [Header("Damage")]
+    public int Damage = 20;
+
+    private PlayerHealth health;
+
+    private void Start() {
+        health = GameObject.Find("Health").GetComponent<PlayerHealth>();
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if(col.collider.name == "Player") {
+            health.DamagePlayer(Damage);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("Health")]
+    public int maxHealth = 100;
+    public int invincibilityFrames = 100; //Not actual frames, fixed update frames 
+
+
+    public int Health {get; set;}
+    public bool Invincible {get; private set;} = false;
+    private int invincibilityFramesCounter;
+
+    private void Start()
+    {
+        Health = maxHealth;
+    }
+
+    /// <summary>
+    /// Checks if the player has died and handles invincibility frames 
+    /// </summary>
+    private void FixedUpdate()
+    {
+        if(Health <= 0) {
+            KillPlayer();
+        }
+
+        if(Invincible) {
+            invincibilityFramesCounter -= 1;
+        }
+        
+        if(invincibilityFramesCounter <= 0) {
+            Invincible = false;
+        }
+    }
+
+    /// <summary>
+    /// Called when the player dies
+    /// </summary>
+    private void KillPlayer() {
+        //TODO
+    }
+
+    /// <summary>
+    /// Damages the player without ignoring the invincibility frames(you can change the health directly if you want to bypass invincibility frames)
+    /// </summary>
+    public void DamagePlayer(int amount) {
+        if(!Invincible) {
+            Health -= amount;
+            invincibilityFramesCounter = invincibilityFrames;
+            Invincible = true;
+        }
+    }
+}
