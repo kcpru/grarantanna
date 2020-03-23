545f47b91110770d62b36fb2cd9e7752cabbd566
Ether141 <majster37@vp.pl>
2020-03-23 21:00:30 +0100

    added parallax background

---
+++ Assets/Scripts/Gameplay/ParallaxBackground.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] private GameObject cam;
+    [SerializeField] private float parallaxEffect;
+
+    private float length;
+    private float startpos;
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float tmp = cam.transform.position.x * (1 - parallaxEffect);
+        float dist = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startpos + dist, cam.transform.position.y, transform.position.z);
+
+        if (tmp > startpos + length)
+            startpos += length;
+        else if (tmp < startpos - length)
+            startpos -= length;
+    }
+}
