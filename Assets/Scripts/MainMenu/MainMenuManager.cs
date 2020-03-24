40cbddbc3d0edea8c5b6c1285ca98bbb55d694ab
Ether141 <majster37@vp.pl>
2020-03-24 14:48:01 +0100

    main menu system

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Pages")]
+    [SerializeField] private GameObject mainPage;
+    [SerializeField] private GameObject levelsPage;
+    [SerializeField] private GameObject creditsPage;
+
+    private void Start() => OpenPage("main");
+
+    public void OpenPage (string pageName)
+    {
+        mainPage.SetActive(false);
+        levelsPage.SetActive(false);
+        creditsPage.SetActive(false);
+
+        switch(pageName)
+        {
+            case "main":
+                mainPage.SetActive(true);
+                break;
+            case "levels":
+                levelsPage.SetActive(true);
+                break;
+            case "credits":
+                creditsPage.SetActive(true);
+                break;
+        }
+    }
+
+    public void StartLevel(int levelIndex) => SceneManager.LoadScene(levelIndex);
+
+    public void ExitGame ()
+    {
+        if(Application.isEditor)
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+        else
+        {
+            Application.Quit(0);
+        }
+    }
+}
