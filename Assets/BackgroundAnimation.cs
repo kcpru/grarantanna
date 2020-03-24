345617b3ce9b1a8c8bfeaeff7714c8be0c44e921
JustABanana <JustABanana@users.noreply.github.com>
2020-03-24 22:51:00 +0100

    Add background to main menu

---
+++ Assets/BackgroundAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAnimation : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        if (transform.position.x > 20) {
+            transform.position = new Vector2(-18f,transform.position.y);
+        }
+        transform.Translate(0.01f,0,0);
+    }
+}
