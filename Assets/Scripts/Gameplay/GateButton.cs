0c4b8c23e08942f3c8fa5bf0eaa84536391fe5d7
Ether141 <majster37@vp.pl>
2020-03-24 19:49:43 +0100

    added gates and buttons

--- Assets/Scripts/Gameplay/GateButton.cs
+++ Assets/Scripts/Gameplay/GateButton.cs
@@ -4,16 +4,29 @@ public class GateButton : MonoBehaviour
 {
     [SerializeField] private Gate gate;
 
+    private GameObject normal;
+    private GameObject pressed;
+
+    private void Start()
+    {
+        normal = transform.GetChild(0).gameObject;
+        pressed = transform.GetChild(1).gameObject;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player") || collision.CompareTag("Object"))
         {
             gate.Open();
+            normal.SetActive(false);
+            pressed.SetActive(true);
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         gate.Close();
+        normal.SetActive(true);
+        pressed.SetActive(false);
     }
 }
