f554a43ebda395424ce8c15e5fea19f06b980cfb
JustABanana <JustABanana@users.noreply.github.com>
2020-03-25 16:52:32 +0100

    Fix the background animation

--- Assets/Scripts/MainMenu/BackgroundAnimation.cs
+++ Assets/Scripts/MainMenu/BackgroundAnimation.cs
@@ -6,7 +6,7 @@ public class BackgroundAnimation : MonoBehaviour
 {
     private void FixedUpdate()
     {
-        if (transform.position.x > 20) {
+        if (transform.position.x > 18) {
             transform.position = new Vector2(-18f,transform.position.y);
         }
         transform.Translate(0.01f,0,0);
