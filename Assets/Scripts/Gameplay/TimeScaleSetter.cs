028b6736107ae98e146e5e55c93b1e764c90935d
JustABanana <JustABanana@users.noreply.github.com>
2020-03-25 21:00:41 +0100

    Add level 5

---
+++ Assets/Scripts/Gameplay/TimeScaleSetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScaleSetter : MonoBehaviour
+{
+
+    [Header("Trampoline force")]
+    public float speed = 0.5f;
+
+    private float savedTimeScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        savedTimeScale = Time.timeScale;
+        Time.timeScale = speed;
+    }
+    private void OnDestroy() {
+        Time.timeScale = savedTimeScale;
+    }
+}
